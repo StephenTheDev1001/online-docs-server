c30f3204d613aca07c7ad1ba108aeb1f6933fc32
Stephen S. Saldana <StephenTheDev@gmail.com>
2022-08-24 09:06:35 -0700

    initial commit

---
+++ server.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose')
+require('dotenv').config()
+const Document = require('./Document')
+const PORT = process.env.PORT || 3001
+
+mongoose.connect(process.env.MONGO_URI)
+
+const io = require('socket.io')(PORT, {
+  cors: {
+    origin: 'https://stephenthedev1001.github.io',
+    methods: ['GET', 'POST']
+  }
+})
+
+const defaultValue = ''
+
+io.on('connection', socket => {
+  socket.on('get-document', async documentId => {
+    const document = await findOrCreateDocument(documentId)
+    socket.join(documentId)
+    socket.emit('load-document', document.data)
+
+    socket.on('send-changes', delta => {
+      socket.broadcast.to(documentId).emit('receive-changes', delta)
+    })
+
+    socket.on('save-document', async data => {
+      await Document.findByIdAndUpdate(documentId, { data })
+    })
+  })
+})
+
+async function findOrCreateDocument(id) {
+  if (id == null) return
+
+  const document = await Document.findById(id)
+  if (document) return document
+  return await Document.create({ _id: id, data: defaultValue })
+}
